66873136a824141762cc46b3daeb2ab6449cbe97
nickl <nickmlanglois@gmail.com>
2019-05-13 13:48:20 -0400

    Test Statements - VertexReferenceGroup Creat. Accept. Tests
    In this commit, I implemented the creational acceptance tests for the
    VertexReferenceGroup and its builder VertexReferenceGroupBuilder.  I was
    working on the Point creational acceptance tests, but needed to complete
    the VertexReferenceGroup tests first.

--- wfparser3/src/acceptance_test/java/com/ht/wfp3/api/statement/acceptance/VertexReferenceGroupAcceptanceTests.java
+++ wfparser3/src/acceptance_test/java/com/ht/wfp3/api/statement/acceptance/VertexReferenceGroupAcceptanceTests.java
@@ -1,11 +1,182 @@
 package com.ht.wfp3.api.statement.acceptance;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+import org.junit.Before;
 import org.junit.Test;
+import com.ht.wfp3.api.statement.GeoVertexReference;
+import com.ht.wfp3.api.statement.NormalVertexReference;
+import com.ht.wfp3.api.statement.StatementFactory;
+import com.ht.wfp3.api.statement.TexVertexReference;
+import com.ht.wfp3.api.statement.VertexReferenceGroup;
+import com.ht.wfp3.api.statement.VertexReferenceGroupBuilder;
 
 public class VertexReferenceGroupAcceptanceTests {
-  
-  // TODO create, copy
+
+  private StatementFactory statementFactory;
+  private VertexReferenceGroupBuilder vertexReferenceGroupBuilder;
+
+  private void assertExpectedIsSetValues(boolean expectedIsGeoRefSet, boolean expectedIsTexRefSet,
+      boolean expectedIsNormalRefSet, VertexReferenceGroup vertexReferenceGroup) {
+    assertEquals(expectedIsGeoRefSet, vertexReferenceGroup.getGeoVertexRef().isSet());
+    assertEquals(expectedIsTexRefSet, vertexReferenceGroup.getTexVertexRef().isSet());
+    assertEquals(expectedIsNormalRefSet, vertexReferenceGroup.getNormalVertexRef().isSet());
+  }
+
+  private void assertValidVertexReferenceGroup(GeoVertexReference geoVertexReference,
+      VertexReferenceGroup vertexReferenceGroup) {
+    assertNotNull(vertexReferenceGroup);
+    assertEquals(geoVertexReference.getVertexIndex(),
+        vertexReferenceGroup.getGeoVertexRef().getVertexIndex());
+    assertExpectedIsSetValues(true, false, false, vertexReferenceGroup);
+  }
+
+  private void assertValidVertexReferenceGroup(GeoVertexReference geoVertexReference,
+      TexVertexReference texVertexReference, VertexReferenceGroup vertexReferenceGroup) {
+    assertNotNull(vertexReferenceGroup);
+    assertEquals(geoVertexReference.getVertexIndex(),
+        vertexReferenceGroup.getGeoVertexRef().getVertexIndex());
+    assertEquals(texVertexReference.getVertexIndex(),
+        vertexReferenceGroup.getTexVertexRef().getVertexIndex());
+    assertExpectedIsSetValues(true, true, false, vertexReferenceGroup);
+
+  }
+
+
+  private void assertValidVertexReferenceGroup(GeoVertexReference geoVertexReference,
+      TexVertexReference texVertexReference, NormalVertexReference normalVertexReference,
+      VertexReferenceGroup vertexReferenceGroup) {
+    assertNotNull(vertexReferenceGroup);
+    assertEquals(geoVertexReference.getVertexIndex(),
+        vertexReferenceGroup.getGeoVertexRef().getVertexIndex());
+    assertEquals(texVertexReference.getVertexIndex(),
+        vertexReferenceGroup.getTexVertexRef().getVertexIndex());
+    assertEquals(normalVertexReference.getVertexIndex(),
+        vertexReferenceGroup.getNormalVertexRef().getVertexIndex());
+    assertExpectedIsSetValues(true, true, true, vertexReferenceGroup);
+  }
+
+
+  private void assertValidVertexReferenceGroup(GeoVertexReference geoVertexReference,
+      NormalVertexReference normalVertexReference, VertexReferenceGroup vertexReferenceGroup) {
+    assertNotNull(vertexReferenceGroup);
+    assertEquals(geoVertexReference.getVertexIndex(),
+        vertexReferenceGroup.getGeoVertexRef().getVertexIndex());
+    assertEquals(normalVertexReference.getVertexIndex(),
+        vertexReferenceGroup.getNormalVertexRef().getVertexIndex());
+    assertExpectedIsSetValues(true, false, true, vertexReferenceGroup);
+  }
+
+  @Before
+  public void setup() {
+    statementFactory = StatementFactory.createStatementFactory();
+    vertexReferenceGroupBuilder = statementFactory.createVertexReferenceGroupBuilder();
+  }
+
+  @Test
+  public void VertexReferenceGroup_createVertexReferenceGroupBuilder_vertexReferenceGroupBuilderIsCreated() {
+    assertNotNull(vertexReferenceGroupBuilder);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void VertexReferenceGroup_geoVertexIsNull_nullPointerExceptionIsThrown() {
+    vertexReferenceGroupBuilder.geoVertexRef(null);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void VertexReferenceGroup_texVertexIsNull_nullPointerExceptionIsThrown() {
+    vertexReferenceGroupBuilder.texVertexRef(null);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void VertexReferenceGroup_normalVertexIsNull_nullPointerExceptionIsThrown() {
+    vertexReferenceGroupBuilder.normalVertexRef(null);
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void VertexReferenceGroup_clearVertexReferenceGroupAndBuild_unsupportedOperationExceptionIsThrown() {
+    vertexReferenceGroupBuilder.clear().build();
+  }
+
+  @Test
+  public void VertexReferenceGroup_setGeoVertexAndBuild_vertexReferenceGroupIsBuilt() {
+    Integer geoVertexIndex = Integer.valueOf(1);
+    GeoVertexReference geoVertexReference =
+        statementFactory.createGeoVertexReference(geoVertexIndex);
+
+    VertexReferenceGroup vertexReferenceGroup =
+        vertexReferenceGroupBuilder.clear().geoVertexRef(geoVertexIndex).build();
+
+    assertValidVertexReferenceGroup(geoVertexReference, vertexReferenceGroup);
+  }
+
+  @Test
+  public void VertexReferenceGroup_setGeoVertexAndTexVertexAndBuild_vertexReferenceGroupIsBuilt() {
+    Integer geoVertexIndex = Integer.valueOf(1);
+    Integer texVertexIndex = Integer.valueOf(-99);
+    GeoVertexReference geoVertexReference =
+        statementFactory.createGeoVertexReference(geoVertexIndex);
+    TexVertexReference texVertexReference =
+        statementFactory.createTexVertexReference(texVertexIndex);
+
+    VertexReferenceGroup vertexReferenceGroup = vertexReferenceGroupBuilder.clear()
+        .geoVertexRef(geoVertexIndex).texVertexRef(texVertexIndex).build();
+
+    assertValidVertexReferenceGroup(geoVertexReference, texVertexReference, vertexReferenceGroup);
+  }
+
+  @Test
+  public void VertexReferenceGroup_setGeoVertexTexVertexAndNormalVertexAndBuild_vertexReferenceGroupIsBuilt() {
+    Integer geoVertexIndex = Integer.valueOf(1);
+    Integer texVertexIndex = Integer.valueOf(-99);
+    Integer normalVertexIndex = Integer.valueOf(56);
+    GeoVertexReference geoVertexReference =
+        statementFactory.createGeoVertexReference(geoVertexIndex);
+    TexVertexReference texVertexReference =
+        statementFactory.createTexVertexReference(texVertexIndex);
+    NormalVertexReference normalVertexReference =
+        statementFactory.createNormalVertexReference(normalVertexIndex);
+
+    VertexReferenceGroup vertexReferenceGroup =
+        vertexReferenceGroupBuilder.clear().geoVertexRef(geoVertexIndex)
+            .texVertexRef(texVertexIndex).normalVertexRef(normalVertexIndex).build();
+
+    assertValidVertexReferenceGroup(geoVertexReference, texVertexReference, normalVertexReference,
+        vertexReferenceGroup);
+  }
+
+  @Test
+  public void VertexReferenceGroup_setGeoVertexAndNormalVertexAndBuild_vertexReferenceGroupIsBuilt() {
+    Integer geoVertexIndex = Integer.valueOf(1);
+    Integer normalVertexIndex = Integer.valueOf(56);
+    GeoVertexReference geoVertexReference =
+        statementFactory.createGeoVertexReference(geoVertexIndex);
+    NormalVertexReference normalVertexReference =
+        statementFactory.createNormalVertexReference(normalVertexIndex);
+
+    VertexReferenceGroup vertexReferenceGroup = vertexReferenceGroupBuilder.clear()
+        .geoVertexRef(geoVertexIndex).normalVertexRef(normalVertexIndex).build();
+
+    assertValidVertexReferenceGroup(geoVertexReference, normalVertexReference,
+        vertexReferenceGroup);
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void VertexReferenceGroup_setTexVertexAndBuild_unsupportedOperationExceptionIsThrown() {
+    vertexReferenceGroupBuilder.clear().texVertexRef(Integer.valueOf(33)).build();
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void VertexReferenceGroup_setNormalVertexAndBuild_unsupportedOperationExceptionIsThrown() {
+    vertexReferenceGroupBuilder.clear().normalVertexRef(Integer.valueOf(-101)).build();
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void VertexReferenceGroup_setTexVertexAndNormalVertexAndBuild_unsupportedOperationExceptionIsThrown() {
+    vertexReferenceGroupBuilder.clear().texVertexRef(Integer.valueOf(22)).normalVertexRef(Integer.valueOf(33)).build();
+  }
+
   // TODO equals, hashCode, compareTo
   // TODO copy malicious mutable statement.
 
--- wfparser3/src/main/java/com/ht/wfp3/api/statement/VertexReferenceGroupBuilderImp.java
+++ wfparser3/src/main/java/com/ht/wfp3/api/statement/VertexReferenceGroupBuilderImp.java
@@ -5,6 +5,9 @@ class VertexReferenceGroupBuilderImp implements VertexReferenceGroupBuilder {
   private Integer geometricVertexReferenceNumber;
   private Integer textureVertexReferenceNumber;
   private Integer normalVertexReferenceNumber;
+  private boolean isGeoRefSet;
+  private boolean isTexRefSet;
+  private boolean isNormalRefSet;
 
   VertexReferenceGroupBuilderImp() {
     super();
@@ -17,38 +20,56 @@ class VertexReferenceGroupBuilderImp implements VertexReferenceGroupBuilder {
     geometricVertexReferenceNumber = null;
     textureVertexReferenceNumber = null;
     normalVertexReferenceNumber = null;
+    isGeoRefSet = false;
+    isTexRefSet = false;
+    isNormalRefSet = false;
     return this;
   }
 
   @Override
   public VertexReferenceGroupBuilder geoVertexRef(Integer geometricVertexReferenceNumber) {
+    if (null == geometricVertexReferenceNumber) {
+      throw new NullPointerException("geometricVertexReferenceNumber cannot be null");
+    }
     this.geometricVertexReferenceNumber = geometricVertexReferenceNumber;
+    isGeoRefSet = true;
     return this;
   }
 
   @Override
   public VertexReferenceGroupBuilder texVertexRef(Integer textureVertexReferenceNumber) {
+    if (null == textureVertexReferenceNumber) {
+      throw new NullPointerException("textureVertexReferenceNumber cannot be null");
+    }
     this.textureVertexReferenceNumber = textureVertexReferenceNumber;
+    isTexRefSet = true;
     return this;
   }
 
   @Override
   public VertexReferenceGroupBuilder normalVertexRef(Integer normalVertexReferenceNumber) {
+    if (null == normalVertexReferenceNumber) {
+      throw new NullPointerException("normalVertexReferenceNumber cannot be null");
+    }
     this.normalVertexReferenceNumber = normalVertexReferenceNumber;
+    isNormalRefSet = true;
     return this;
   }
 
   @Override
   public VertexReferenceGroup build() {
-    GeoVertexReference geometricVertexReference = (geometricVertexReferenceNumber != null
+    if (!isGeoRefSet) {
+      throw new UnsupportedOperationException("geometricVertexReferenceNumber must be set at least");
+    }
+    GeoVertexReference geometricVertexReference = (isGeoRefSet
         ? statementFactory.createGeoVertexReference(geometricVertexReferenceNumber)
         : statementFactory.createGeoVertexReference(VertexReferenceImp.INDEX_NOT_SET_VALUE));
-    TexVertexReference textureVertexReference = (textureVertexReferenceNumber != null
+    TexVertexReference textureVertexReference = (isTexRefSet
         ? statementFactory.createTexVertexReference(textureVertexReferenceNumber)
         : statementFactory.createTexVertexReference(VertexReferenceImp.INDEX_NOT_SET_VALUE));
-    NormalVertexReference normalVertexReference = (normalVertexReferenceNumber != null
+    NormalVertexReference normalVertexReference = (isNormalRefSet
         ? statementFactory.createNormalVertexReference(normalVertexReferenceNumber)
-        : statementFactory.createNormalVertexReference(VertexReferenceImp.INDEX_NOT_SET_VALUE));
+        : statementFactory.createDisabledNormalVertexReference());
     return new VertexReferenceGroupImp(geometricVertexReference, textureVertexReference,
         normalVertexReference);
   }
