7308cd30486714e550cfd36eb6baa7898b85c3aa
nickl <nickmlanglois@gmail.com>
2019-06-25 08:53:50 -0400

    Localizer - Start LocalizerType Tests
    In this commit, I started the LocalizerType tests.

---
+++ wfparser3/src/acceptance_test/java/com/ht/l10n/acceptance/LocalizerTypeTest.java
@@ -0,0 +1,14 @@
+package com.ht.l10n.acceptance;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class LocalizerTypeTest {
+  
+  @Test
+  public void LocalizerType_createFactories_factoriesCreated() {
+    fail("Not yet implemented");
+  }
+  
+  
+}
