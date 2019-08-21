8b97e21acafbf775c00910b5208acc6992d1f80f
nickl <nickmlanglois@gmail.com>
2019-08-21 14:24:25 -0400

    Message - Delete the Connect Subsystem
    In this commit, I deleted the connect subsystem.  The design was
    terrible to non-existent.  It is too soon to implement the system.  I'll
    wait until I have a better idea of exactly how I need to define the
    interactions between subsystems before proceeding with the design and
    implementation.

--- wfparser3/src/main/java/com/ht/connect/ConnectorFactory.java
+++
@@ -1,9 +0,0 @@
-package com.ht.connect;
-
-public interface ConnectorFactory {
-  static ConnectorFactory createConnectorFactory() {
-    return new ConnectorFactoryImp();
-  }
-
-  Connector createConnector();
-}
--- wfparser3/src/main/java/com/ht/connect/ConnectorFactoryImp.java
+++
@@ -1,9 +0,0 @@
-package com.ht.connect;
-
-public class ConnectorFactoryImp implements ConnectorFactory {
-
-  @Override
-  public Connector createConnector() {
-    return new ConnectorImp();
-  }
-}
